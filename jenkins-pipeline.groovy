838e776105813d012b3b6a28bff5ea178e489793
ELGUMOS1 <54557959+ELGUMOS1@users.noreply.github.com>
2019-12-11 00:00:13 -0600

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -13,7 +13,6 @@ node {
      stage('Integration Test') {
        sh "rm /usr/local/bin/docker-compose"
        sh "curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose" 
-       sh "ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose"
        sh "chmod +x docker-compose"
        sh "mv docker-compose /usr/local/bin" 
        sh "/usr/local/bin/docker-compose -f docker-ci-demo/docker-compose-ci-test.yaml up -d"
